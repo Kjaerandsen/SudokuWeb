ee273dc5fcae18572a84b7a785d56da6ac1c1f0e
jonakj <jonakj@stud.ntnu.no>
2023-10-07 16:19:00 +0200

    Basic initial layout template.

--- sudoku-web/src/app/about/page.js
+++ sudoku-web/src/app/about/page.js
@@ -1,5 +1,3 @@
-import Image from 'next/image'
-
 export default function Page() {
     return(
         <h1>Hello this is an about page</h1>
--- sudoku-web/src/app/layout.js
+++ sudoku-web/src/app/layout.js
@@ -1,10 +1,10 @@
 import 'bootstrap/dist/css/bootstrap.css'
-import './globals.css'
-import { Inter } from 'next/font/google'
+//import './globals.css'
+//import { Inter } from 'next/font/google'
 import Link from 'next/link'
 //import { useEffect } from 'react'
 
-const inter = Inter({ subsets: ['latin'] })
+//const inter = Inter({ subsets: ['latin'] })
 
 export const metadata = {
   title: 'Sudoku Web',
@@ -21,10 +21,10 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <body>
       <nav class="navbar navbar-expand-lg navbar-light bg-light border-bottom border-black">
-          <Link href="/">
-            <p class="navbar-brand">Sudoku Web</p>
-          </Link>
-        </nav>
+        <Link href="/">
+          <p class="navbar-brand">Sudoku Web</p>
+        </Link>
+      </nav>
       {children}
       </body>
     </html>
