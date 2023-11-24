bfcd3d13d8b8fa62722c7be571d545e33fe3212a
jonakj <jonakj@stud.ntnu.no>
2023-11-24 12:58:52 +0100

    Added basic PWA functionality.

--- src/app/layout.js
+++ src/app/layout.js
@@ -9,11 +9,42 @@ import Link from 'next/link'
 export const metadata = {
   title: 'Sudoku Web',
   description: 'Sudoku web game using Next',
+  manifest: "/manifest.json",
+  appleWebApp: {
+    capable: true,
+    statusBarStyle: "default",
+    title: "Sudoku Web",
+    // startUpImage: [],
+  },
+  formatDetection: {
+    telephone: false,
+  },
+  openGraph: {
+    type: "website",
+    siteName: "Sudoku Web",
+    title: {
+      default: "Sudoku Web",
+      template: "Sudoku Web PWA",
+    },
+    description: "Sudoku web game using Next",
+  },
+  twitter: {
+    card: "summary",
+    title: {
+      default: "Sudoku Web",
+      template: "Sudoku Web - PWA",
+    },
+    description: "Sudoku web game using Next",
+  },
 }
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <head>
+        <meta name="theme-color" content="#703061" />
+        <link rel="manifest" href="/manifest.json" />
+      </head>
       <body className='bg-slate-800 md:bg-gray-950'>
       <nav className="bg-gray-700 p-3 border-b-4 border-fuchsia-900">
         <div className='flex items-center justify-between container m-auto'>
